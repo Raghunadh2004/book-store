ab4d24a65e4e6be6f3e099036aed731c9b299327
Raghunadh2004 <Maturi.Raghunadh@iiitb.ac.in>
2024-12-01 22:39:04 +0530

    mm

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',  // Exposes the server to the host machine and network
-    port: 5173,       // You can change this if needed
-    strictPort: true, // Ensures Vite fails if the port is already in use
-    watch: {
-      usePolling: true, // Required for some Docker setups to detect file changes
-    },
-  },
   plugins: [react()],
 })
