5b268a52657142865a7d458e11334fdd5097f14e
Raghunadh2004 <Maturi.Raghunadh@iiitb.ac.in>
2024-12-01 21:10:35 +0530

    t1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',   // Expose Vite server on all network interfaces
-    port: 5173,        // Use port 5173
-    watch: {
-      usePolling: true,  // Use polling to avoid file system issues in Docker
-      ignored: ['**/node_modules/**', '**/.git/**', '/proc/**', '/sys/**'] // Ignore unnecessary paths
-    }
-  }
 })
+
