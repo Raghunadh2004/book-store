aa9562f2d98d3714a2c81e97f6e667fc39b03f0b
Raghunadh2004 <Maturi.Raghunadh@iiitb.ac.in>
2024-12-09 23:51:44 +0530

    changed base backend url

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,7 +1,13 @@
 const getBaseUrl = () => {
-    return window.location.hostname === 'localhost' ? 
-    'http://localhost:5000' : 
-    'http://192.168.49.2:30002';
-}
+    const hostname = window.location.hostname;
+
+    if (hostname === 'localhost') {
+        return 'http://localhost:5000'; // Local development environment
+    } else if (hostname === 'book-store') {
+        return 'http://book-store/backend'; // Ingress hostname with backend path
+    } else {
+        return 'http://192.168.49.2:30002'; // Default to the NodePort service
+    }
+};
 
 export default getBaseUrl;
