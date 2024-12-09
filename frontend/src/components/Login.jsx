fc28f66a1398fd85610355f464bf75f9c3f1c4d3
Raghunadh2004 <Maturi.Raghunadh@iiitb.ac.in>
2024-12-09 12:05:17 +0530

    Updated jenkins file

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
         watch,
         formState: { errors },
     } = useForm()
-    console.log(import.meta.env.VITE_FIREBASE_API_KEY); // Vite
+    // console.log(import.meta.env.VITE_FIREBASE_API_KEY); // Vite
 
     const onSubmit = async (data) =>{
         try {
