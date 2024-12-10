1fa46ba37f71400a020d1b1a1a923624e5f7b5d3
Raghunadh2004 <Maturi.Raghunadh@iiitb.ac.in>
2024-12-10 16:50:37 +0530

    mm

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Redirect to backend on localhost:5000
+        changeOrigin: true,
+        secure: false, // Only use if the backend uses HTTPS with a self-signed certificate
+      },
+    },
+  },
 })
