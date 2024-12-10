66ab866fc0e577eaeb3b8c9cfc5891984e79bb0a
Raghunadh2004 <Maturi.Raghunadh@iiitb.ac.in>
2024-12-10 11:43:24 +0530

    m

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -3,9 +3,6 @@ const getBaseUrl = () => {
 
     if (hostname === 'localhost') {
         return 'http://localhost:5000'; // Local development environment
-    } else if (hostname === 'book-store') {
-        console.log('http://book-store/backend');
-        return 'http://api.book-store'; // Ingress hostname with backend path
     } else {
         return 'http://192.168.49.2:30002'; // Default to the NodePort service
     }
